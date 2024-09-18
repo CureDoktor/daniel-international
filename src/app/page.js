7c91b3339080b2a545bb7cc01f45e4d022a84ade
Nikola Ćurčić <62150474+CureDoktor@users.noreply.github.com>
2024-09-18 20:13:03 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Academy from "./components/Academy";
 
 export default function Home() {
   return (
-    <div className=" max-w-7xl px-3 md:px-0  items-center min-h-screen pb-20  m-auto">
+    <div className=" max-w-7xl px-3 md:px-0  items-center pb-20  m-auto">
       <Academy />
     </div>
   );
